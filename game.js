2b05689c7d6e290e39b4ef7138a87c6250a96bc0
McLoyf <alex.ortiz242005@gmail.com>
2026-02-04 23:03:15 -0600

    Update gameController
    Right now validate doesn't work the way it ought to, will work on it later

--- game.js
+++ game.js
@@ -15,5 +15,28 @@ const createPlayer = (name, mark) => {
 };
 
 const gameController = (function() {
+    // Validate currently doesn't work how I think it should work, right now it does nothing
+    // TODO: fix validate logic!
+    
+    const validate = index => {
+    if (Gameboard.getBoard(index) === "X" || Gameboard.getBoard(index)){
+        throw new Error("Position taken!");
+    }
+};
+    const equis = (index, mark) => {
+        validate(index);
+        return Gameboard.setMark(index, mark);
+    };
+    const o = (index, mark) => {
+        validate(index);
+        return Gameboard.setMark(index, mark);
+    };
 
+    return { equis, o };
 })();
+
+const testPlayer = createPlayer("TEST", "X");
+
+gameController.equis(3, "X");
+gameController.o(3, "O");
+console.log(Gameboard.getBoard());
