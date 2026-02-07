aabf23a59e0879772f68682d66ded7d47596ede1
McLoyf <alex.ortiz242005@gmail.com>
2026-02-06 21:08:24 -0600

    Create getState
    Winning and ties now work
    
    I'm sure that there is a more elegant solution to do this but this is good enough for now (Also I couldn't think of something better)

--- game.js
+++ game.js
@@ -1,5 +1,5 @@
 const Gameboard = (function() {
-    const board = ['', '', '','', '', '', '', '', ''];
+    const board = ['', '', '', '', '', '', '', '', ''];
 
     const getBoardIndex = (index) => board[index];
     const getBoard = () => board;
@@ -20,13 +20,63 @@ const gameController = (function() {
     if (Gameboard.getBoardIndex(index) === "X" || Gameboard.getBoardIndex(index) === "O"){
         throw new Error("Position taken!");
     }
-};
+}
+    const getState = () => {
+        if (Gameboard.getBoardIndex(0) === "X" && Gameboard.getBoardIndex(1) === "X" && Gameboard.getBoardIndex(2) == "X"){
+            return 1;
+        } else if (Gameboard.getBoardIndex(3) === "X" && Gameboard.getBoardIndex(4) === "X" && Gameboard.getBoardIndex(5) == "X"){
+            return 1;
+        } else if (Gameboard.getBoardIndex(6) === "X" && Gameboard.getBoardIndex(7) === "X" && Gameboard.getBoardIndex(8) == "X"){
+            return 1;
+        } else if (Gameboard.getBoardIndex(0) === "X" && Gameboard.getBoardIndex(3) === "X" && Gameboard.getBoardIndex(6) == "X"){
+            return 1;
+        } else if (Gameboard.getBoardIndex(1) === "X" && Gameboard.getBoardIndex(4) === "X" && Gameboard.getBoardIndex(7) == "X"){
+            return 1;
+        } else if (Gameboard.getBoardIndex(2) === "X" && Gameboard.getBoardIndex(5) === "X" && Gameboard.getBoardIndex(8) == "X"){
+            return 1;
+        } else if (Gameboard.getBoardIndex(0) === "X" && Gameboard.getBoardIndex(4) === "X" && Gameboard.getBoardIndex(8) == "X"){
+            return 1;
+        } else if (Gameboard.getBoardIndex(2) === "X" && Gameboard.getBoardIndex(4) === "X" && Gameboard.getBoardIndex(6) == "X"){
+            return 1;
+        } else if (Gameboard.getBoardIndex(0) === "O" && Gameboard.getBoardIndex(1) === "O" && Gameboard.getBoardIndex(2) == "O"){
+            return 2;
+        } else if (Gameboard.getBoardIndex(3) === "O" && Gameboard.getBoardIndex(4) === "O" && Gameboard.getBoardIndex(5) == "O"){
+            return 2;
+        } else if (Gameboard.getBoardIndex(6) === "O" && Gameboard.getBoardIndex(7) === "O" && Gameboard.getBoardIndex(8) == "O"){
+            return 2;
+        } else if (Gameboard.getBoardIndex(0) === "O" && Gameboard.getBoardIndex(3) === "O" && Gameboard.getBoardIndex(6) == "O"){
+            return 2;
+        } else if (Gameboard.getBoardIndex(1) === "O" && Gameboard.getBoardIndex(4) === "O" && Gameboard.getBoardIndex(7) == "O"){
+            return 2;
+        } else if (Gameboard.getBoardIndex(2) === "O" && Gameboard.getBoardIndex(5) === "O" && Gameboard.getBoardIndex(8) == "O"){
+            return 2;
+        } else if (Gameboard.getBoardIndex(0) === "O" && Gameboard.getBoardIndex(4) === "O" && Gameboard.getBoardIndex(8) == "O"){
+            return 2;
+        } else if (Gameboard.getBoardIndex(2) === "O" && Gameboard.getBoardIndex(4) === "O" && Gameboard.getBoardIndex(6) == "O"){
+            return 2;
+        } else {
+            if (!Gameboard.getBoard().includes("")){
+                return 3;
+            }
+            return 0;
+        }
+    }
+
     const markBoard = (index, mark) => {
         validate(index);
         return Gameboard.setMark(index, mark);
-    };
+    }
 
     const decideFate = () => {
+        if (getState() === 0){
+            return ("Game is still ongoing");
+        } else if(getState() === 1){
+            throw new Error("X wins");
+        } else if(getState() === 2){
+            throw new Error("O wins");
+        } else if (getState() === 3){
+            throw new Error("Tie Game!");
+        }
     }
 
     return { markBoard, decideFate };
@@ -34,6 +84,20 @@ const gameController = (function() {
 
 const testPlayer = createPlayer("TEST", "X");
 
-// gameController.markBoard(3, "O");
-// gameController.markBoard(3, "X");
+/* Gameboard
+   0  1  2  x  o  x
+   3  4  5  o  x  o
+   6  7  8  o  x  o
+*/
+gameController.markBoard(0, "X");
+gameController.markBoard(1, "O");
+gameController.markBoard(2, "X");
+gameController.markBoard(3, "O");
+gameController.markBoard(4, "X");
+gameController.markBoard(5, "O");
+gameController.markBoard(6, "O");
+gameController.markBoard(7, "X");
+gameController.markBoard(8, "O");
 // console.log(Gameboard.getBoard());
+// console.log(gameController.getState());
+console.log(gameController.decideFate());
